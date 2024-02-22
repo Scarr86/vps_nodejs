4a36532a90647d52c11c08ad7e700d5ab971873c
Denis <voland2114@gmail.com>
2024-02-22 13:08:39 +0700

    second commit

---
+++ server.js
@@ -0,0 +1,26 @@
+// подключение express
+const express = require("express");
+// создаем объект приложения
+const app = express();
+
+const data = [
+    {
+        _id: 1,
+        body: "sdfsdfsdfqw41234"
+    },
+    {
+        _id: 2,
+        body: "231bnjklh34k2b4bk"
+    }
+];
+
+
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
+    response.send("<h2>Привет Express!</h2>");
+});
+app.get("/data", function(request, response){
+    response.send(JSON.stringify(data));
+});
+// начинаем прослушивать подключения на 3000 порту
+app.listen(3000);
